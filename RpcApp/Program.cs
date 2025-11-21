2f14bfa7baea7df8db07302585331d5a8aa11fe1
kisshka <tanakazankina862@gmail.com>
2025-11-22 01:00:47 +0300

    был реализован механизм подписки на управление устройством

---
+++ RpcApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using RpcApp.Domain;
+using System.Net;
+using System.Text;
+using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        //Необходимо для того чтобы не было ошибки с кодировкой
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        var subscriber = new OrionSubscribe();
+        subscriber.Subscribe();
+        /*  для ручного тестирования
+                subscriber.DebugRawHttp();*/
+    }
+}
